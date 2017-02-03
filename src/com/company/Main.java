563f6c8188e4c47d94f12f82980c45977570015e
rteach <ross.c.teach@gmail.com>
2017-02-02 19:02:26 -0500

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,85 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scnr = new Scanner(System.in);
+
+    public static int getDieSize(String prompt) {
+        int num;
+
+        do {
+            System.out.println(prompt);
+            while (!scnr.hasNextInt()) {
+                System.out.println("Input error");
+                System.out.println(prompt);
+                scnr.nextLine();
+            }
+            num = scnr.nextInt();
+            if (num <=0){
+                System.out.println("Input error");
+                scnr.nextLine();
+            }
+
+        } while (num <=0);
+
+        return num;
+    }
+
+    public static void isSpecialRoll (int die1, int die2){
+
+        if (die1 == 1 && die2 == 1) {
+            System.out.println("Snake Eyes");
+        } else if (die1 == 6 && die2 == 6) {
+            System.out.println("Box Cars");
+        } else if (die1 + die2 == 7) {
+            System.out.println("Crap Out");
+        }
+    }
+
+    public static int rollDieMethodOne (int numDieSides){
+        int die = (int) (Math.random()*numDieSides+1);
+        return die;
+    }
+
+    public static int rollDieMethodTwo (int numDieSides){
+        Random random = new Random();
+        int die = random.nextInt(numDieSides)+1;
+        return die;
+    }
+
+
+    public static void displayRoll (int die1, int die2, int rollCounter){
+
+        System.out.println("Roll " + rollCounter + ":");
+        System.out.println(die1);
+        System.out.println(die2);
+        isSpecialRoll(die1, die2);
+
+    }
+
+
+    public static void main(String[] args) {
+        String input = null;
+        int rollCounter = 1;
+
+        do {
+            int numSides = getDieSize("Enter number of die sides");
+            int die1 = rollDieMethodTwo(numSides);
+            int die2 = rollDieMethodTwo(numSides);
+            displayRoll(die1, die2, rollCounter);
+            rollCounter++;
+            System.out.println("Continue (Y/N)");
+            scnr.nextLine();
+            input = scnr.nextLine();
+            while (!input.equalsIgnoreCase("y") && !input.equalsIgnoreCase("n")) {
+                System.out.println("Input error");
+                System.out.println("Continue? (Y/N)");
+                input = scnr.nextLine();
+            }
+        } while (input.equalsIgnoreCase("y"));
+
+        }
+    }
+
